b43c73f7058e9f90ba66a6fc074df77fa354481f
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-13 09:51:19 -0400

    fix teams ingestion to check across all seasons

--- backend/ingestions/teams.py
+++ backend/ingestions/teams.py
@@ -1,5 +1,4 @@
 import asyncio
-from datetime import date
 
 import httpx
 import psycopg
@@ -14,27 +13,24 @@ async def run():
         async with httpx.AsyncClient() as client:
             season_response = await client.get(seasons_url)
             season_response.raise_for_status()
+            seasons = season_response.json()["SiteKit"]["Seasons"]
 
-        seasons = season_response.json()["SiteKit"]["Seasons"]
-        # Fallback to 5 for season id if the date check fails
-        current_season_id = 5
-        for season in seasons:
-            if (
-                date.fromisoformat(season["start_date"])
-                <= date.today()
-                <= date.fromisoformat(season["end_date"])
-            ):
-                current_season_id = season["season_id"]
+            teams: dict[str, dict] = {}
+            for season in seasons:
+                teams_url = f"https://lscluster.hockeytech.com/feed/index.php?feed=modulekit&view=teamsbyseason&season_id={season['season_id']}&key=446521baf8c38984&client_code=pwhl"
+                response = await client.get(teams_url)
+                response.raise_for_status()
+                for team in response.json()["SiteKit"]["Teamsbyseason"]:
+                    teams[team["id"]] = {
+                        "api_id": team["id"],
+                        "name": team["name"],
+                        "city": team["city"],
+                        "code": team["code"],
+                        "nickname": team["nickname"],
+                        "logo_url": team["team_logo_url"],
+                    }
 
-        teams_url = f"https://lscluster.hockeytech.com/feed/index.php?feed=modulekit&view=teamsbyseason&season_id={current_season_id}&key=446521baf8c38984&client_code=pwhl"
-
-        async with httpx.AsyncClient() as client:
-            response = await client.get(teams_url)
-            response.raise_for_status()
-
-        response = response.json()
-
-        for team in response["SiteKit"]["Teamsbyseason"]:
+        for team in teams.values():
             await conn.execute(
                 """
                 INSERT INTO teams (api_id, name, city, code, nickname, logo_url)
@@ -47,12 +43,12 @@ async def run():
                     logo_url = EXCLUDED.logo_url
             """,
                 (
-                    team["id"],
+                    team["api_id"],
                     team["name"],
                     team["city"],
                     team["code"],
                     team["nickname"],
-                    team["team_logo_url"],
+                    team["logo_url"],
                 ),
             )
 
